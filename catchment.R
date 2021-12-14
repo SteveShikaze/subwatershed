dce9ffea58ff5898c012fdf3dac67d9d19926dda
maseology <maseology@pm.me>
2021-12-14 16:35:28 -0500

    u

---
+++ catchment.R
@@ -0,0 +1,44 @@
+
+
+library(shiny)
+library(leaflet)
+library(jsonlite)
+
+
+shinyApp(
+  ui = fluidPage(
+    leafletOutput("map")
+  ),
+
+  server = function(input, output, session) {
+
+    ormgp.region <- readOGR("shp/ORMGP-region.geojson",verbose = FALSE)
+    
+    output$map <- renderLeaflet({
+     leaflet(ormgp.region) %>%
+      addProviderTiles( providers$Stamen.TonerLite, options = providerTileOptions(noWrap = TRUE) ) %>%
+      addPolygons(fill = FALSE)
+    })
+
+    observe({
+      leafletProxy("map") %>% clearPopups()
+      event <- input$map_click
+      if (is.null(event)) return()
+      lat <- round(event$lat,3)
+      lng <- round(event$lng, 3)
+      
+      showNotification(paste0("querying to ",lat, ', ', lng))
+      qstr <- paste0("http://golang.oakridgeswater.ca:8080/carea/",event$lat,"/",event$lng)
+      geojson <- readLines(qstr) %>% paste(collapse = "\n")
+      gg <- fromJSON(geojson)
+      isolate(leafletProxy("map") %>%
+                clearGeoJSON() %>%
+                addPopups(event$lng,event$lat,paste0(lat, ', ', lng,'<br>Area ', gg$features$properties$area,'km²'))
+              ) %>%
+        addGeoJSON(geojson)
+    })
+    
+    session$onSessionEnded(stopApp)
+  }#,
+  # options = list(height = 600)
+)
